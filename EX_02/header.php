3bc0f91b23a4175b74bec2e0df87452b74a014f1
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-06-29 11:47:05 +0200

    jour 5

---
+++ EX_02/header.php
@@ -0,0 +1,13 @@
+<header>
+            <div class="align">
+            <h1>Summer Code Camp</h1>
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="vitrine-accueil.php"> Accueil </a></li>
+                    <li><a href="vitrine-programme.php"> Programme </a></li>
+                    <li><a href="vitrine-contacts.php"> Contact </a></li>
+                </ul>
+             </nav>
+        </header>
+        
